11159053987e370578f998509d31212094ba53df
nickhill <nickhill@us.ibm.com>
2018-05-22 12:00:42 -0700

    update ComposeTrustManagerFactory to work with single-server authority
    also add single-arg constructor

--- src/main/java/com/ibm/etcd/client/config/ComposeTrustManagerFactory.java
+++ src/main/java/com/ibm/etcd/client/config/ComposeTrustManagerFactory.java
@@ -18,12 +18,15 @@ package com.ibm.etcd.client.config;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.ManagerFactoryParameters;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 import org.slf4j.Logger;
@@ -44,8 +47,15 @@ public class ComposeTrustManagerFactory extends SimpleTrustManagerFactory {
     private static final Logger logger = LoggerFactory.getLogger(ComposeTrustManagerFactory.class);
 
     private final TrustManager tm;
+    
+    private final X509TrustManager defaultTm;
+    
+    public ComposeTrustManagerFactory(String deploymentName)
+            throws CertificateException, IOException  {
+        this(deploymentName, deploymentName, null);
+    }
 
-    public ComposeTrustManagerFactory(String name, final String deployment,
+    public ComposeTrustManagerFactory(String name, final String deploymentName,
             ByteSource certSource) throws CertificateException, IOException {
         super(name);
 
@@ -57,8 +67,10 @@ public class ComposeTrustManagerFactory extends SimpleTrustManagerFactory {
                 cert = (X509Certificate) certFactory.generateCertificate(in);
             }
         }
+        
+        this.defaultTm = getDefaultTrustManager(cert);
 
-        tm = new X509TrustManager() {
+        this.tm = new X509TrustManager() {
             @Override
             public void checkClientTrusted(X509Certificate[] chain, String str) {
                 logger.info("Accepting a client certificate: " + chain[0].getSubjectDN() );
@@ -67,19 +79,25 @@ public class ComposeTrustManagerFactory extends SimpleTrustManagerFactory {
             @Override
             public void checkServerTrusted(X509Certificate[] chain, String str) throws CertificateException {
                 final X509Certificate serverCert = chain[0];
-                if ( !serverCert.getSubjectDN().getName().equalsIgnoreCase(deployment)
-                        && !serverCert.getSubjectDN().getName().equalsIgnoreCase("CN=" + deployment) ) {
+                boolean ok = false;
+                if(defaultTm != null) try {
+                    // first try regular trustmanager
+                    defaultTm.checkServerTrusted(chain, str);
+                    ok = true;
+                } catch(CertificateException ce) {}
+                if( !ok && !serverCert.getSubjectDN().getName().equalsIgnoreCase(deploymentName)
+                        && !serverCert.getSubjectDN().getName().equalsIgnoreCase("CN=" + deploymentName) ) {
                     throw new CertificateException("Certificate with unknown deployment: "
                             + serverCert.getSubjectDN().getName());
                 }
-                if ( cert != null ) {
+                if( cert != null ) {
                     if ( !serverCert.getIssuerDN().equals(cert.getIssuerDN()) ) {
                         throw new CertificateException("Certificate Issuers do not match: "
                                 + serverCert.getIssuerDN());
 
                     }
                     // See if certs are the same, if so we are good.
-                    if ( !serverCert.equals(cert) ) try {
+                    if( !serverCert.equals(cert) ) try {
                         // Not your CA's. Check if it has been signed by your CA
                         serverCert.verify(cert.getPublicKey());
                     } catch ( Exception exc ) {
@@ -101,6 +119,25 @@ public class ComposeTrustManagerFactory extends SimpleTrustManagerFactory {
                 return cert != null ? new X509Certificate[] { cert } : EmptyArrays.EMPTY_X509_CERTIFICATES;
             }
         };
+    }
+    
+    private static X509TrustManager getDefaultTrustManager(X509Certificate cert) {
+        try {
+            TrustManagerFactory defaultTmf = TrustManagerFactory
+                    .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            KeyStore ks = null;
+            if(cert != null) {
+                ks = KeyStore.getInstance(KeyStore.getDefaultType());
+                ks.setCertificateEntry("compose-provided", cert);
+            }
+            defaultTmf.init(ks);
+            if(defaultTmf != null) for(TrustManager m : defaultTmf.getTrustManagers()) {
+                if(m instanceof X509TrustManager) return (X509TrustManager) m;
+            }
+        } catch (NoSuchAlgorithmException | KeyStoreException e) {
+            logger.warn("Failed to look up default TrustManager", e);
+        }
+        return null;
     }
     
     @Override
