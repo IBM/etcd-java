4e2188528ff63cb39633d7162faf58be618bc229
nickhill <nickhill@us.ibm.com>
2019-09-23 21:06:07 -0700

    Download etcd server from github instead of google storage (which broke)

--- build-env/install-etcd.sh
+++ build-env/install-etcd.sh
@@ -8,8 +8,9 @@ ETCD_VERSION=v3.3.15
 INSTALL_DIR="${1:-$HOME/etcd}"
 
 # Download and install etcd
+echo "Downloading etcd server ${ETCD_VERSION}"
 cd /tmp
-wget -q https://storage.googleapis.com/etcd/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
+wget -nv https://github.com/etcd-io/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
 mkdir -p ${INSTALL_DIR}
 tar xzf etcd-${ETCD_VERSION}-linux-amd64.tar.gz -C ${INSTALL_DIR} --strip-components=1
 rm -rf etcd*.gz
