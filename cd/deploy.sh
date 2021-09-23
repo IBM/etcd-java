7f91bad25ca94a83c111b355042f0965a1f6c43c
nickhill <nickhill@us.ibm.com>
2021-09-22 17:19:49 -0700

    fix: Update (re-)encrypted signing key
    Also update version of maven-gpg-plugin

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -5,8 +5,8 @@ if [ "$TRAVIS_BRANCH" = 'main' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "
     echo "deploying release to central repository"
 
     # prepare key for signing
-    openssl aes-256-cbc -K "$encrypted_171b1c559d7b_key" -iv "$encrypted_171b1c559d7b_iv" -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_80546c16ab97_key -iv $encrypted_80546c16ab97_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+    gpg --fast-import --batch cd/signingkey.asc
     shred --remove cd/signingkey.asc
     
     # sign and deploy to central repository
